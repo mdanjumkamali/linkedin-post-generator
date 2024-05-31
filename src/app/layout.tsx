4d5413e99d2b5fc41d52bd64cf7f280893064fe7
Md Anjum Kamali <mdanjumkamali@gmail.com>
2024-05-31 15:55:48 +0530

    Add OG

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,29 @@ import "./globals.css";
 const inter = Roboto_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "LinkedIn Post Generator",
+  metadataBase: new URL("https://linkedinpostgenerator.vercel.app"),
+  title: {
+    default: "LinkedIn Post Generator",
+    template: "%s | LinkedIn Post Generator",
+  },
   description: "Boost your LinkedIn presence with our Post Generator",
+  openGraph: {
+    title: "LinkedIn Post Generator",
+    description: "Boost your LinkedIn presence with our Post Generator",
+    url: "https://linkedinpostgenerator.vercel.app",
+    siteName: "LinkedIn Post Generator",
+    locale: "en_US",
+    type: "website",
+    images: "https://linkedinpostgenerator.vercel.app/icon.png",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "LinkedIn Post Generator",
+    description: "Boost your LinkedIn presence with our Post Generator",
+    //site: "@YourTwitterHandle", //Replace with your Twitter handle if available
+    //creator: "@YourTwitterHandle", // Replace with your Twitter handle if available
+    images: "https://linkedinpostgenerator.vercel.app/icon.png",
+  },
 };
 
 export default function RootLayout({
@@ -18,26 +39,6 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/icon.png" />
-        <meta property="og:title" content="LinkedIn Post Generator" />
-        <meta
-          property="og:description"
-          content="Boost your LinkedIn presence with our Post Generator"
-        />
-        <meta property="og:image" content="/icon.png" />
-        <meta
-          property="og:url"
-          content="https://linkedinpostgenerator.vercel.app"
-        />
-        <meta property="og:type" content="website" />
-        <meta property="og:site_name" content="LinkedIn Post Generator" />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="LinkedIn Post Generator" />
-        <meta
-          name="twitter:description"
-          content="Boost your LinkedIn presence with our Post Generator"
-        />
-        <meta name="twitter:image" content="/icon.png" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
